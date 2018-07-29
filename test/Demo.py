62a465b4ef7ad6275838e7c563622c805e5270a6
rafaelh <rafaelh@student.ethz.ch>
2018-07-29 11:48:19 +0200

    include TremauxSolver in Demo Fixes #14

--- test/Demo.py
+++ test/Demo.py
@@ -10,48 +10,50 @@ import drawers.ASCIIDrawer as ASCIIDrawer
 import solvers.RightWallFollowerSolver as RightWallFollowerSolver
 import solvers.LeftWallFollowerSolver as LeftWallFollowerSolver
 import solvers.RandomMouseSolver as RandomMouseSolver
+import solvers.TremauxSolver as TremauxSolver
 
 class Demo:
 
     size = 10
     drawer = ASCIIDrawer.ASCIIDrawer()
-    print("testing the KruskalGenerator and RightWallFollowerSolver")
+    print("Showcasing the KruskalGenerator and RightWallFollowerSolver")
     generator = KruskalGenerator.KruskalGenerator()
     solver = RightWallFollowerSolver.RightWallFollowerSolver()
     maze = generator.generateRandomMaze(size)
     path = solver.solveMaze(maze)
     drawer.drawMaze(maze)
     drawer.drawPath(maze, path)
-    print("Test finished")
+    print("Showcase finished")
 
-    print("testing the DepthFirstSearchGenerator and LeftWallFollowerSolver")
+    print("Showcasing the DepthFirstSearchGenerator and LeftWallFollowerSolver")
     generator = DepthFirstSearchGenerator.DepthFirstSearchGenerator()
     solver = LeftWallFollowerSolver.LeftWallFollowerSolver()
     maze = generator.generateRandomMaze(size)
     path = solver.solveMaze(maze)
     drawer.drawMaze(maze)
     drawer.drawPath(maze,path)
-    print("Test finished")
+    print("Showcase finished")
 
-    print("testing the PrimGenerator and RandomMouseSolver")
+    print("Showcasing the PrimGenerator and RandomMouseSolver")
     generator = PrimGenerator.PrimGenerator()
     solver = RandomMouseSolver.RandomMouseSolver()
     maze = generator.generateRandomMaze(size)
-    #path = solver.solveMaze(maze)
+    path = solver.solveMaze(maze)
     drawer.drawMaze(maze)
-    #drawer.drawPath(maze, path)
-    print("Test finished")
+    drawer.drawPath(maze, path)
+    print("Showcase finished")
 
-    """testing the RecursiveDivisionGenerator"""
-    print("testing the RecursiveDivisionGenerator")
+    print("Showcasing the RecursiveDivisionGenerator and TremauxSolver")
     generator = RecursiveDivisionGenerator.RecursiveDivisionGenerator()
+    solver = TremauxSolver.TremauxSolver()
     maze = generator.generateRandomMaze(size)
+    path = solver.solveMaze(maze)
     drawer.drawMaze(maze)
-    print("RecursiveDivisionTest finished")
+    drawer.drawPath(maze, path)
+    print("Showcasine finished")
 
-    """testing the BinaryTreeGenerator"""
-    print("testing the BinaryTreeGenerator")
+    print("Showcasing the BinaryTreeGenerator")
     generator = BinaryTreeGenerator.BinaryTreeGenerator()
     maze = generator.generateRandomMaze(size)
     drawer.drawMaze(maze)
-    print("All tests finished")
+    print("All Showcases finished")
