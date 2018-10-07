cda8149a28fc04716ca8f9c87be3335c9ab23002
rafaelh <rafaelh@student.ethz.ch>
2018-10-07 21:36:34 +0200

    Add ability to write drawer output into a file Fixes #26

--- drawers/ASCIIDrawer.py
+++ drawers/ASCIIDrawer.py
@@ -8,65 +8,65 @@ class ASCIIDrawer(AbstractDrawer.AbstractDrawer):
         AbstractDrawer.AbstractDrawer.__init__(self)
         self.log = logging.getLogger(__name__)
 
-    def drawMaze(self, maze):
+    def drawMaze(self, maze, filepath=None):
         """implement a drawing algorithm"""
-        size = maze.getSize()
-        sys.stdout.write("\n ")
-        for i in range (0, size):
-            if not maze.getCell(0, i).getTop().isRemoved():
-                sys.stdout.write("_ ")
-            else:
-                sys.stdout.write("  ")
-        print("")
-        for j in range (0, size):
-            if not maze.getCell(j, 0).getLeft().isRemoved():
-                sys.stdout.write("|")
-            else:
-                sys.stdout.write(" ")
-            for i in range (0,size):
-                if not maze.getCell(j, i).getBottom().isRemoved():
-                    sys.stdout.write("_")
+        with self.openWriter(filepath) as out:
+            size = maze.getSize()
+            out.write("\n ")
+            for i in range (0, size):
+                if not maze.getCell(0, i).getTop().isRemoved():
+                    out.write("_ ")
                 else:
-                    sys.stdout.write(" ")
-                if not maze.getCell(j, i).getRight().isRemoved():
-                    sys.stdout.write("|")
+                    out.write("  ")
+            out.write("\n")
+            for j in range (0, size):
+                if not maze.getCell(j, 0).getLeft().isRemoved():
+                    out.write("|")
                 else:
-                    sys.stdout.write(" ")
-            print("")
+                    out.write(" ")
+                for i in range (0,size):
+                    if not maze.getCell(j, i).getBottom().isRemoved():
+                        out.write("_")
+                    else:
+                        out.write(" ")
+                    if not maze.getCell(j, i).getRight().isRemoved():
+                        out.write("|")
+                    else:
+                        out.write(" ")
+                out.write("\n")
 
-    def drawPath(self, maze, path):
+    def drawPath(self, maze, path, filepath=None):
         """print path with ASCII signs"""
-        for i in range (0, len(path)):
-            sys.stdout.write("(" + path[i].getX().__str__() + "," + path[i].getY().__str__() + ") ")
-        print("")
-        sys.stdout.write("( ")
-        # Draw entrance
-        entrance = maze.getEntrance()
-        if (entrance.getLeft().isRemoved() and maze.getLeftNeighbour(entrance) == None):
-            sys.stdout.write("-> ")
-        else:
-            sys.stdout.write("v ")
-        for i in range (0, len(path)-1):
-            x1 = path[i].getX()
-            x2 = path[i+1].getX()
-            y1 = path[i].getY()
-            y2 = path[i+1].getY()
-            if (x1 < x2):
-                sys.stdout.write("v ")
-            elif (x1 > x2):
-                sys.stdout.write("^ ")
-            elif (y1 < y2):
-                sys.stdout.write("-> ")
-            elif (y1 > y2):
-                sys.stdout.write("<- ")
+        with self.openWriter(filepath) as out:
+            for i in range (0, len(path)):
+                out.write("(" + path[i].getX().__str__() + "," + path[i].getY().__str__() + ") ")
+            out.write("\n( ")
+            # Draw entrance
+            entrance = maze.getEntrance()
+            if (entrance.getLeft().isRemoved() and maze.getLeftNeighbour(entrance) == None):
+                out.write("-> ")
             else:
-                self.log.error('Invalid cell transition')
-                raise Exception('Invalid cell transition')
-        # Draw exit
-        exit = maze.getExit()
-        if (exit.getRight().isRemoved() and maze.getRightNeighbour(exit) == None):
-            sys.stdout.write("-> ")
-        else:
-            sys.stdout.write(" v")
-        sys.stdout.write(")")
-        print("")
+                out.write("v ")
+            for i in range (0, len(path)-1):
+                x1 = path[i].getX()
+                x2 = path[i+1].getX()
+                y1 = path[i].getY()
+                y2 = path[i+1].getY()
+                if (x1 < x2):
+                    out.write("v ")
+                elif (x1 > x2):
+                    out.write("^ ")
+                elif (y1 < y2):
+                    out.write("-> ")
+                elif (y1 > y2):
+                    out.write("<- ")
+                else:
+                    self.log.error('Invalid cell transition')
+                    raise Exception('Invalid cell transition')
+            # Draw exit
+            exit = maze.getExit()
+            if (exit.getRight().isRemoved() and maze.getRightNeighbour(exit) == None):
+                out.write("-> ")
+            else:
+                out.write(" v")
+            out.write(")\n")
--- drawers/AbstractDrawer.py
+++ drawers/AbstractDrawer.py
@@ -1,4 +1,6 @@
 import logging
+import sys
+import contextlib
 
 class AbstractDrawer:
 
@@ -6,8 +8,20 @@ class AbstractDrawer:
         self.log = logging.getLogger(__name__)
         self.log.debug("generating a drawer")
 
-    def drawMaze(self,maze):
+    def drawMaze(self, maze, filepath):
         """implement a drawing algorithm for the maze"""
 
-    def drawPath(self,maze,path):
+    def drawPath(self, maze, path, filepath):
         """implement a drawing algorithm for the path"""
+
+    @contextlib.contextmanager
+    def openWriter(self, filepath=None):
+        if filepath:
+            out = open(filepath, 'w')
+        else:
+            out = sys.stdout
+        try:
+            yield out
+        finally:
+            if out != sys.stdout:
+                out.close()
