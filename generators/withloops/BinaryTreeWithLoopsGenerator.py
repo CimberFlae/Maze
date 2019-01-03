8598cff89a81fd03b4883c04f8479a1e45ee7e7c
rafaelh <rafi.haeuselmann@hispeed.ch>
2019-01-03 20:52:22 +0100

    #43 - fix loop creating in BinaryTreeWithLoopsGenerator

--- generators/withloops/BinaryTreeWithLoopsGenerator.py
+++ generators/withloops/BinaryTreeWithLoopsGenerator.py
@@ -13,4 +13,4 @@ class BinaryTreeWithLoopsGenerator(BinaryTreeGenerator):
         # Don't want too many loops, so we set the probability for a cell to remove both walls to 1/(2*size)
         if self.random.random() <= 1/(2*size):
             cell.remove_left()
-            cell.remove_left()
+            cell.remove_top()
